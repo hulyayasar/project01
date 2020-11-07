09a0bb8aeb24f407f10f06813c752bc935e91dad
hulyayasar <73432891+hulyayasar@users.noreply.github.com>
2020-11-07 20:51:43 +0200

    second commit from local master

--- src/firstPackage/FIrstClass.java
+++ src/firstPackage/FIrstClass.java
@@ -4,7 +4,7 @@ public class FIrstClass {
 
 	public static void main(String[] args) {
 		System.out.println("First commit from local master");
-
+		System.out.println("Second line of code I am typing");
 	}
 
 }
